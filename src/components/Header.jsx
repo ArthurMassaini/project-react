130894d6d207848f11c20e65f05d61d9414b8bcf
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-03-06 19:45:40 -0300

    finalizando header com icones certos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,14 +12,10 @@ function Header({ type }) {
         </Link>
       </header>
     );
-  } else {
+  } else if (type === 'cart') {
     return (
       <header className="header">
-        {type === 'cart' ? (
-          <h1>Bem Vindo ao carrinho!</h1>
-        ) : (
-          <h1>Bem Vindo ao checkout!</h1>
-        )}
+        <h1>Bem Vindo ao carrinho!</h1>
         <Link to="/">
           <img
             src="https://findicons.com/files/icons/1580/devine_icons_part_2/256/home.png"
@@ -28,6 +24,25 @@ function Header({ type }) {
             height="72px"
           />
         </Link>
+      </header>
+    );
+  } else if (type === 'checkout') {
+    return (
+      <header className="header">
+        <h1>Bem Vindo ao checkout!</h1>
+        <div>
+          <Link to="/cart">
+            <img src={cartIcon} alt="cart" />
+          </Link>
+          <Link to="/">
+            <img
+              src="https://findicons.com/files/icons/1580/devine_icons_part_2/256/home.png"
+              alt="home"
+              witdth="90px"
+              height="72px"
+            />
+          </Link>
+        </div>
       </header>
     );
   }
