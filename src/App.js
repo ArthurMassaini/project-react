0a4534a4b6e8eb5ab5c446327bb844d1e33c88ff
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-03-06 15:16:35 -0300

    importando as pages para renderizacao inicial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Cart from './pages/Home';
+import Checkout from './pages/Checkout';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route exact path="/cart" component={Cart}/>
-      <Route exact path="/cart" component={Checkout}/>
-      <Route component={NotFound}/>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/cart" component={Cart} />
+      <Route exact path="/cart/checkout" component={Checkout} />
+      <Route component={NotFound} />
     </Switch>
   );
 }
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function NotFound() {
-  return <main>SORRY... NOT WAS FOUND</main>;
+  return <main>SORRY... NOTHING WAS FOUND</main>;
 }
 
 export default NotFound;
