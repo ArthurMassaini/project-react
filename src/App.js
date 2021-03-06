7e993e0ccbe4d854327e6781fcf2ed024f3b5481
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-03-06 15:08:31 -0300

    colocando route e switch no app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
+import { Switch. Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/cart" component={Cart}/>
+      <Route component={NotFound}/>
+    </Switch>
   );
 }
 
