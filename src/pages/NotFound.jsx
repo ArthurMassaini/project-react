ee4e353721cd3159360ebfbb52ab7dbbcdf95b10
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-03-07 18:00:13 -0300

    adicionando uma pagina caso nada foi encontrado

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function NotFound() {
-  return <main>SORRY... NOTHING WAS FOUND</main>;
+  return (
+    <main className="main center">
+      <h1>SORRY... NOTHING WAS FOUND</h1>
+      <img
+        src="https://www.iconpacks.net/icons/2/free-sad-face-icon-2691-thumb.png"
+        alt="sad-face"
+      />
+    </main>
+  );
 }
 
 export default NotFound;
