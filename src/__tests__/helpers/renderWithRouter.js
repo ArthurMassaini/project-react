67ce526a2457750e3d8a04926420c5ac63f36b8a
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-03-07 19:52:13 -0300

    teste se renderiza os games

--- src/__tests__/helpers/renderWithRouter.js
+++ src/__tests__/helpers/renderWithRouter.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, Switch } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
+import rootReducer from '../../redux/reducers/index';
 
-const renderWithRouter = (component) => {
-  const history = createMemoryHistory();
-  return {
-    ...render(<Router history={history}>{component}</Router>),
-    history,
-  };
-};
+const renderWithReduxAndRouter = (
+  routes,
+  {
+    initialState = {},
+    store = createStore(rootReducer, initialState, applyMiddleware(thunk)),
+  } = {},
+  {
+    route = '/',
+    history = createMemoryHistory({ initialEntries: [route] }),
+  } = {},
+) => ({
+  ...render(
+    <Provider store={store}>
+      <Router history={history}>
+        <Switch>{routes}</Switch>
+      </Router>
+    </Provider>,
+  ),
+  store,
+  history,
+});
 
-export default renderWithRouter;
+export default renderWithReduxAndRouter;
