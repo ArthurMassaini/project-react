34324cdb9f0db8a97c8e064e96c26307dd85582b
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-03-06 21:00:34 -0300

    Actions de ordenaçao

--- src/redux/reducers/products.js
+++ src/redux/reducers/products.js
@@ -13,6 +13,51 @@ const products = (state = INITIAL_STATE, action) => {
       return { ...state, loading: false };
     case TYPES.RETRIEVE_GAMES:
       return { ...state, games: [...action.games] };
+    case TYPES.PRICE:
+      if (action.id === 1) {
+        action.games.sort((a, b) => {
+          if (a.price < b.price) return 1;
+          if (a.price > b.price) return -1;
+          return 0;
+        });
+      } else {
+        action.games.sort((a, b) => {
+          if (a.price < b.price) return -1;
+          if (a.price > b.price) return 1;
+          return 0;
+        });
+      }
+      return { ...state, games: [...action.games] };
+    case TYPES.NAME:
+      if (action.id === 1) {
+        action.games.sort((a, b) => {
+          if (a.name < b.name) return -1;
+          if (a.name > b.name) return 1;
+          return 0;
+        });
+      } else {
+        action.games.sort((a, b) => {
+          if (a.name < b.name) return 1;
+          if (a.name > b.name) return -1;
+          return 0;
+        });
+      }
+      return { ...state, games: [...action.games] };
+    case TYPES.RATING:
+      if (action.id === 1) {
+        action.games.sort((a, b) => {
+          if (a.score < b.score) return 1;
+          if (a.score > b.score) return -1;  
+          return 0;
+        });
+      } else {
+        action.games.sort((a, b) => {
+          if (a.score < b.score) return -1;
+          if (a.score > b.score) return 1;
+          return 0;
+        });
+      }
+      return { ...state, games: [...action.games] };
     default:
       return state;
   }
