1cc5fded9134eb1de5f6f8a8797aa4ec098df872
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-03-08 15:48:08 -0300

    adicionando teste de ordenacao

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { cleanup, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { renderWithRouterAndStore } from './helpers/renderWithRouterAndStore';
 import App from '../App';
 
@@ -14,7 +15,24 @@ describe('Requiriments:', () => {
 
   it('should order the cards by price, name and score', async () => {
     renderWithRouterAndStore(<App />, '/');
-    const cards = await waitFor(() => screen.getAllByAltText(/game-card/i));
-    expect(cards.length).toBe(9);
+
+    const beforeOrder = await waitFor(() => screen.getAllByAltText(/game-card/i));
+    expect(beforeOrder[0]).toHaveAttribute('src', 'super-mario-odyssey.png');
+
+    const dropdown = screen.getByTestId(/order/i);
+
+    userEvent.selectOptions(dropdown, 'Preço: alto a baixo');
+    const afterOrderByPrice = await waitFor(() => screen.getAllByAltText(/game-card/i));
+    expect(afterOrderByPrice[0]).toHaveAttribute('src', 'call-of-duty-wwii.png');
+
+    userEvent.selectOptions(dropdown, 'Nome: A-Z');
+    const afterOrderByName = await waitFor(() => screen.getAllByAltText(/game-card/i));
+    expect(afterOrderByName[0]).toHaveAttribute('src', 'call-of-duty-infinite-warfare.png');
+
+    userEvent.selectOptions(dropdown, 'Nota: alto a baixo');
+    const afterOrderByScore = await waitFor(() => screen.getAllByAltText(/game-card/i));
+    expect(afterOrderByScore[0]).toHaveAttribute('src', 'shards-of-darkness.png');
+
   });
+  
 });
